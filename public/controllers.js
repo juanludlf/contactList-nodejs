4df0ec3265bc44e7c3490e4e83c6cefcf8b73f55
Juanlu de la Fuente <jdelafuente@us.es>
2016-05-03 18:30:25 +0200

    Fixed refresh problem and node_modules removed

--- public/controllers.js
+++ public/controllers.js
@@ -1,27 +1,30 @@
-var myApp = angular.module("ContactListApp",[]);
+var myApp = angular.module("ContactListApp", []);
 
-myApp.controller('AppCtrl',['$scope','$http',function($scope,$http){
-	console.log("Controller initialized");
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Controller initialized");
 
-	var refresh = function (){
-		$http.get('/contacts').success(function (contacts){
-			console.log('Data received successfully');
-			$scope.contactlist = contacts;
-		});
-	}
+    var refresh = function() {
+        $http.get('/contacts').success(function(contacts) {
+            console.log('Data received successfully');
+            $scope.contactlist = contacts;
+        });
+    }
 
-	refresh();
+    refresh();
 
-	$scope.addContact = function(){
-		console.log("Inserting contact ...");
-		$http.post('/contacts',$scope.contact);
-		refresh();
-	}
+    $scope.addContact = function() {
+        console.log("Inserting contact ...");
+        $http.post('/contacts', $scope.contact).then(function() {
+            refresh();
+        });
 
-	$scope.deleteContact = function(name){
-		console.log("Deleting contact with "+name);
-		$http.delete('/contacts/'+name);
-		refresh();
-	}
+    }
+
+    $scope.deleteContact = function(name) {
+        console.log("Deleting contact with " + name);
+        $http.delete('/contacts/' + name).then(function() {
+            refresh();
+        });
+    }
 
 }]);
--- server.js
+++ server.js
@@ -8,83 +8,87 @@ var port = (process.env.PORT || 10000);
 
 var app = express();
 
-var dbFileName  = path.join(__dirname,'contacts.json');
+var dbFileName = path.join(__dirname, 'contacts.json');
 var db = new DataStore({
-		filename : dbFileName,
-		autoload: true
-	});
+    filename: dbFileName,
+    autoload: true
+});
 
 console.log('DB initialized');
 
-db.find({},function (err,contacts){
+db.find({}, function(err, contacts) {
 
-	if(contacts.length == 0){
-		console.log('Empty DB, loading initial data');
+    if (contacts.length == 0) {
+        console.log('Empty DB, loading initial data');
 
-		person1 = {
-			name : 'Pablo',
-			email : 'pafmon@gmail.com',
-			number: '682 123 123'
-		};
+        person1 = {
+            name: 'Pablo',
+            email: 'pafmon@gmail.com',
+            number: '682 123 123'
+        };
 
-		person2 = {
-			name : 'Pedro',
-			email : 'pedro@gmail.com',
-			number: '682 122 126'
-		};
+        person2 = {
+            name: 'Pedro',
+            email: 'pedro@gmail.com',
+            number: '682 122 126'
+        };
 
-		db.insert([person1, person2]);
+        db.insert([person1, person2]);
 
-	}else{
-		console.log('DB has '+contacts.length+' contacts ');
-	}
+    } else {
+        console.log('DB has ' + contacts.length + ' contacts ');
+    }
 
 });
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-app.get('/contacts',function(req,res){
-	console.log('New GET request');
+app.get('/contacts', function(req, res) {
+    console.log('New GET request');
 
-	db.find({},function (err,contacts){
-		res.json(contacts);
-	});
+    db.find({}, function(err, contacts) {
+        res.json(contacts);
+    });
 });
 
-app.post('/contacts',function(req,res){
-	console.log('New POST request');
-	console.log(req.body);
-	db.insert(req.body);
-	res.sendStatus(200);
+app.post('/contacts', function(req, res) {
+    console.log('New POST request');
+    console.log(req.body);
+    db.insert(req.body);
+    res.sendStatus(200);
 });
 
-app.get('/contacts/:name',function(req,res){
-	var n = req.params.name;
-	console.log('New GET request for contact with name '+n);
+app.get('/contacts/:name', function(req, res) {
+    var n = req.params.name;
+    console.log('New GET request for contact with name ' + n);
 
-	db.find({ name : n},function (err,contacts){
-		console.log("Contacts obtained: "+contacts.length);
-		if(contacts.length  > 0){
-			res.send(contacts[0]);
-		}else{
-			res.sendStatus(404);
-		}
-	});
+    db.find({
+        name: n
+    }, function(err, contacts) {
+        console.log("Contacts obtained: " + contacts.length);
+        if (contacts.length > 0) {
+            res.send(contacts[0]);
+        } else {
+            res.sendStatus(404);
+        }
+    });
 });
 
-app.delete('/contacts/:name',function(req,res){
-	var n = req.params.name;
-	console.log('New DELETE request for contact with name '+n);
+app.delete('/contacts/:name', function(req, res) {
+    var n = req.params.name;
+    console.log('New DELETE request for contact with name ' + n);
 
-	db.remove({ name: n},{}, function(err,numRemoved){
-		console.log("Contacts removed: "+numRemoved);
-		if(numRemoved  == 1)
-			res.sendStatus(200);
-		else
-			res.sendStatus(404);
-	});
+    db.remove({
+        name: n
+    }, {}, function(err, numRemoved) {
+        console.log("Contacts removed: " + numRemoved);
+        if (numRemoved == 1)
+            res.sendStatus(200);
+        else
+            res.sendStatus(404);
+    });
 });
 
 app.listen(port);
-console.log('Magic is happening on port '+port);
+console.log('Magic is happening on port ' + port);
